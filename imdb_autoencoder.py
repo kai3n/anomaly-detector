f2fc7eebc0abbd1de70aae2df2d19a46d4ed42e8
Mingun Pak <mingunpa@usc.edu>
2017-06-26 21:25:44 -0700

    add:

--- imdb_autoencoder.py
+++ imdb_autoencoder.py
@@ -7,14 +7,15 @@ import torch.nn.functional as F
 
 from models import EncoderRNN, DecoderRNN, AttnDecoderRNN
 from train import evaluate
+import train
 
 class ImdbAutoEncoder(object):
     def __init__(self):
         pass
 
     def autoencoder(self, sentence):
-        encoder = torch.load('test_encoder_4.715040796774099', map_location={'cuda:0': 'cpu'})
-        decoder = torch.load('test_decoder_4.715040796774099', map_location={'cuda:0': 'cpu'})
+        encoder = torch.load('test_encoder_imdb10000_0.0037651115254803288', map_location={'cuda:0': 'cpu'})
+        decoder = torch.load('test_decoder_imdb10000_0.0037651115254803288', map_location={'cuda:0': 'cpu'})
 
         output_words, attentions = evaluate(
             encoder, decoder, sentence)
@@ -23,6 +24,11 @@ class ImdbAutoEncoder(object):
 
 if __name__ == '__main__':
     imdb_autoencoder = ImdbAutoEncoder()
+    criterion = nn.NLLLoss()
     while True:
         text = input()
-        print(imdb_autoencoder.autoencoder(text))
+        text = text[:15]
+        decoded_text = imdb_autoencoder.autoencoder(text)
+        anomaly_predict_val = 1.0-(len(set(text.split()) - set(decoded_text)))/len(text.split())
+        print(decoded_text)
+        print('Probability of genuine movie review: {}%'.format(anomaly_predict_val*100))
