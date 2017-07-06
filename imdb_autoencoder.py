c93b744fc6ab1b7661a87ce023ecba46bf0ea1df
Mingun Pak <mingunpa@usc.edu>
2017-07-05 17:02:38 -0700

    add: val_test, show_val_test

--- imdb_autoencoder.py
+++ imdb_autoencoder.py
@@ -17,19 +17,51 @@ class ImdbAutoEncoder(object):
         self.output_lang = output_lang
 
     def autoencoder(self, sentence):
-        encoder = torch.load('test_encoder_imdb10000_0.0037651115254803288', map_location={'cuda:0': 'cpu'})
-        decoder = torch.load('test_decoder_imdb10000_0.0037651115254803288', map_location={'cuda:0': 'cpu'})
+        encoder = torch.load('test_encoder_imdb100000_0.3082789914441499_2.0', map_location={'cuda:0': 'cpu'})
+        decoder = torch.load('test_decoder_imdb100000_0.3082789914441499_2.0', map_location={'cuda:0': 'cpu'})
 
         output_words, _, loss = evaluate(
             encoder, decoder, sentence, self.input_lang, self.output_lang)
 
         return output_words, loss
 
+def val_test(textfile):
+    lines = [line.rstrip('\n') for line in open(textfile)]
+    total_loss = 0
+    for line in lines[:100]:
+        line = normalizeString(line)
+        line = ' '.join(line.split()[:14])
+        decoded_text, decoded_loss = imdb_autoencoder.autoencoder(line)
+        total_loss += decoded_loss
+    return total_loss / len(lines)
+
+def show_val_test():
+    performance = []
+    for i in range(4, 15):
+        performance.append(val_test('imdb_len_{}.txt'.format(str(i))))
+
+    import matplotlib.pyplot as plt
+    plt.rcdefaults()
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    objects = ('Len 4', 'Len 5', 'Len 6', 'Len 7', 'Len 8', 'Len 9', 'Len 10', 'Len 11', 'Len 12',
+               'Len 13', 'Len 14')
+    y_pos = np.arange(len(objects))
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('SGD Loss')
+    plt.title('Len & Loss Graph')
+    plt.show()
+
 
 if __name__ == '__main__':
 
-    input_lang, output_lang, pairs = prepareData('eng', 'eng', False)
+    input_lang, output_lang, pairs, _ = prepareData('eng', 'eng', False)
     imdb_autoencoder = ImdbAutoEncoder(input_lang, output_lang)
+    # show_val_test()
+
     criterion = nn.NLLLoss()
     while True:
         text = input()
